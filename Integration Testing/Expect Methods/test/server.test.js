3845fe61bc913885eb57fb749c13c5863a91edd6
Suraj <surajchan68@gmai.com>
2020-04-22 21:43:37 +0545

    finishing almost testing

--- Integration Testing/Expect Methods/test/server.test.js
+++ Integration Testing/Expect Methods/test/server.test.js
@@ -61,3 +61,13 @@ describe("Color Code Converter API", function () {
     });
 
 });
+
+/** For Exporting Server :)
+ * chai.request(app)
+ *   .put('/user/me')
+ *   .send({ password: '123', confirmPassword: '123' })
+ *   .end(function (err, res) {
+ *      expect(err).to.be.null;
+ *      expect(res).to.have.status(200);
+ *   });
+ */
